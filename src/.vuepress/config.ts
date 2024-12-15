d0b0fa3195995b7e7dd1ea4f1885ae7b0b664e3d
615898744asd <615898744@qq.com>
2024-12-15 19:16:06 +0800

    feat：优化增加网站图标

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,14 +3,15 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
+  head: [
+     [
+         'link', // 设置 favicon.ico，注意图片放在 public 文件夹下
+         { rel: 'icon', href: 'logo.png' }
+     ]
+    ],
   base: "/LiWeb/",
   lang: "zh-CN",
   title: "博客演示",
-  head: [
-    [
-      'link',{ rel: 'icon', href: '/favicon.ico' }
-    ]
-  ],
   description: "vuepress-theme-hope 的博客演示",
 
   theme,
