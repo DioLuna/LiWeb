53afa41f33bb5d7f85e4cb1babe7acac62fc4670
615898744asd <615898744@qq.com>
2024-12-15 18:50:23 +0800

    feat：增加图标展示

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -7,7 +7,9 @@ export default defineUserConfig({
   lang: "zh-CN",
   title: "博客演示",
   head: [
-    ['link',{ rel:'icon',href:'/images/logo.png' }]
+    [
+      'link',{ rel: 'icon', href: '/favicon.ico' }
+    ]
   ],
   description: "vuepress-theme-hope 的博客演示",
 
