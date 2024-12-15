c2e2e3551417cab2c65728c257be906dbe36b323
615898744asd <615898744@qq.com>
2024-12-15 18:41:56 +0800

    feat：设置网站图标

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -4,9 +4,11 @@ import theme from "./theme.js";
 
 export default defineUserConfig({
   base: "/LiWeb/",
-
   lang: "zh-CN",
   title: "博客演示",
+  head: [
+    ['link',{ rel:'icon',href:'/images/logo.png' }]
+  ],
   description: "vuepress-theme-hope 的博客演示",
 
   theme,
